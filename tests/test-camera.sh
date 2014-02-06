e445211ef15d997f394edba289048333a6bab99d
William Manley <will@stb-tester.com>
2014-02-06 15:40:15 +0000

    WIP: Smart TV support: Add command stbt camera validate
    stbt camera validate is intended to measure the quality of video capture
    that a camera (plus the software corrections to that camera) provides.  It
    does this by displaying images on the TV and then compares the output to
    what was expected producing a report.
    
    Currently it defers responsibility for displaying the reference images on
    the TV to the user, but support for automatic TV control using DLNA is
    coming in the future.
    
    TODO:
    * Validate that tests are sufficient?
    * Makefile stuff to install validator
    * Don't hard-code paths
    * Split TV drivers into different python file

--- tests/test-camera.sh
+++ tests/test-camera.sh
@@ -46,3 +46,95 @@ stop_fake_video_src() {
     unset FAKE_VIDEO_SRC_PID
     rm uri_playlist gst-shm-socket
 }
+
+###
+### stbt camera validate tests
+###
+
+run_validation() {
+    color="$1"
+    extra=${2:-identity}
+    stbt-run \
+        --source-pipeline="filesrc location=$testdir/$1.png ! pngdec ! videoconvert ! $extra ! video/x-raw,width=1280,height=720 ! imagefreeze" \
+        --control=none \
+        $srcdir/extra/camera/stbt-camera-validate.py --tv-driver=assume "$1"
+}
+
+test_that_validation_passes_on_pristine_input() {
+    run_validation letters-bw "" || fail "Validation failed on pristine input"
+}
+test_that_validation_fails_if_letters_are_offset() {
+    run_validation letters-bw "videobox top=-2 left=-2 ! \
+                       videobox autocrop=true" \
+        && fail "Validation succeeded on invalid input"
+    return 0
+}
+test_that_validation_fails_if_letters_are_scaled_down() {
+    run_validation letters-bw "videoscale ! video/x-raw,width=1278,height=718 ! \
+                       videobox autocrop=true" \
+        && fail "Validation succeeded on invalid input"
+    return 0
+}
+test_that_validation_fails_if_letters_are_scaled_up() {
+    run_validation letters-bw "videoscale ! video/x-raw,width=1282,height=722 ! \
+                       videobox autocrop=true" \
+        && fail "Validation succeeded on invalid input"
+    return 0
+}
+test_that_validation_fails_with_vignetting() {
+    run_validation letters-bw \
+        "rsvgoverlay location=$testdir/vignette-overlay.svg" \
+        && fail "Validation succeeded on invalid input"
+    return 0
+}
+
+# Test manual driver
+
+test_that_validation_video_served_over_http_is_correct() {
+    # We test a lot of functionality in this test.  Arguably it should be split
+    # down.  We test that:
+    #
+    # * HTTP URLs are provided.
+    # * Videos can be played from those URLs.
+    # * The videos are what stbt camera validate was expecting and thus that
+    #   the videos have been generated on demand successfully.
+    # * The validation code is hooked up to the drivers.
+    # * The manual driver responds to user input (pressing <ENTER>).
+    #
+    # The setup looks like:
+    #
+    #  |-----------------|          video         |----------------------|
+    #  | fakevideosrc.py | --- gst-shm-socket --> | stbt camera validate |
+    #  |-----------------|                        |----------------------|
+    #          ^ stdin                               ^ stdin        | stderr
+    #          |                                     |         instructions
+    #          |                            stbt_validate_input     |
+    #          |                                     |              V
+    #          |                                  |----------------------|
+    #    uri_playlist ----------------------------|      this test       |
+    #                                             |----------------------|
+    #
+    # The test listens for instructions from stbt camera validate's stderr and
+    # instructs fakevideosrc.py to display URIs before telling stbt camera
+    # validate to proceed by pressing enter on it's stdin
+    start_fake_video_src
+
+    mkfifo stbt_validate_input
+    while cat stbt_validate_input; do true; done | \
+        stbt run \
+            --source-pipeline="$fake_video_src_source" \
+            --control=none \
+        $srcdir/extra/camera/stbt-camera-validate.py --driver=manual 2>&1 | (\
+        while read line; do
+            if [[ "$line" =~ 'http://' ]]; then
+                fake_video_src_show "$(echo "$line" | grep -Eo 'http://\S*')"
+            fi
+            if [[ "$line" =~ 'Press <ENTER>' ]]; then
+                sleep 1
+                echo >stbt_validate_input
+            fi
+        done
+    )
+
+    stop_fake_video_src
+}
