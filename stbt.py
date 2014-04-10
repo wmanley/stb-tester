wmanley/stb-tester

commit b8d39d434bd374f46cef4c5970a486e6c1b60de6

Allow user_pipeline to output compressed video
This commit relaxes the requirements of the user provided source pipeline.
Previously it had to provide raw video frames whereas now stbt does
decoding itself.  This is convenient for users but the real advantage is
that it allows us to buffer more intelligently.

raw video takes up lots of memory but it's very easy to manipulate.  You
can drop any frame (`GstBuffer`) and it will have no effect on the meaning
of any previous or subsequent frames.  Compressed video is the opposite.
If you drop buffers in an h264 stream you will see artefacts until the next
I frame comes along.

stbt has some queues set to leaky as our processing can be slower than the
rate at which frames arrive.  It is usually more important that the video
frames being analysed are recent rather than consecutive so we drop frames.
With this commit we are a little more selective and ensure that we only
drop decoded frames rather than compressed ones to avoid artefacts, even
under load.  This makes the assumption that your machine can comfortable
decode video in real time.

This is required to implement smart-tv-support where we wish to operate on
decoded frames but want to avoid dropping any frames that we've done
expensive transformations on - if we have to drop frames under load we
prefer to do it as early as possible in the pipeline to waste the minimum
amount of CPU time.  With this we will be able to insert an additional
transformation pipeline between the leaky queue and stbt's algorithms.

This feature also lays the groundwork for a performance measurement mode
where you don't want to drop any frames as you want to analyse them all,
for instance to measure animation smoothness.  This should now be possible
without having to buffer (potentially GBs of) raw frames in memory.

This is still not perfect.  Ideally in the future we will be able to drop
frames even before decoding them if we can identify that dropping them will

stbt.py
@@ -1138,13 +1138,17 @@ class Display(object):
         self.restart_source_enabled = restart_source
 
         appsink = (
-            "appsink name=appsink max-buffers=1 drop=true sync=false "
+            "appsink name=appsink max-buffers=1 drop=false sync=true "
             "emit-signals=true "
             "caps=video/x-raw,format=BGR")
         self.source_pipeline_description = " ! ".join([
             user_source_pipeline,
-            "queue leaky=downstream name=q",
-            "videoconvert",
+            'queue name=_stbt_user_data_queue max-size-buffers=0 '
+            '    max-size-bytes=0 max-size-time=10000000000',
+            "decodebin",
+            'videoconvert',
+            'video/x-raw,format=BGR',
+            'queue name=_stbt_raw_frames_queue max-size-buffers=2',
             appsink])
         self.create_source_pipeline()
 
@@ -1179,6 +1183,14 @@ class Display(object):
         debug("source pipeline: %s" % self.source_pipeline_description)
         debug("sink pipeline: %s" % sink_pipeline_description)
 
+        if (self.source_pipeline.set_state(Gst.State.PAUSED)
+                == Gst.StateChangeReturn.NO_PREROLL):
+            # This is a live source, drop frames if we get behind
+            self.source_pipeline.get_by_name('_stbt_raw_frames_queue') \
+                .set_property('leaky', 'downstream')
+            self.source_pipeline.get_by_name('appsink') \
+                .set_property('sync', False)
+
         self.source_pipeline.set_state(Gst.State.PLAYING)
         self.sink_pipeline.set_state(Gst.State.PLAYING)
 
@@ -1200,7 +1212,8 @@ class Display(object):
         if self.restart_source_enabled:
             # Handle loss of video (but without end-of-stream event) from the
             # Hauppauge HDPVR capture device.
-            source_queue = self.source_pipeline.get_by_name("q")
+            source_queue = self.source_pipeline.get_by_name(
+                "_stbt_user_data_queue")
             self.start_timestamp = None
             source_queue.connect("underrun", self.on_underrun)
             source_queue.connect("running", self.on_running)

extra/vm/setup-vagrant-user.sh
@@ -37,14 +37,14 @@ mkdir -p ~/.config/stbt
 cat > ~/.config/stbt/stbt.conf <<-EOF
 	[global]
 	# Source pipeline for the Hauppauge HD PVR video-capture device.
-	#source_pipeline = v4l2src device=/dev/video0 ! tsdemux ! video/x-h264 ! decodebin
+	#source_pipeline = v4l2src device=/dev/video0 ! tsdemux ! h264parse
 	
 	# Handle loss of video (but without end-of-stream event) from the video
 	# capture device. Set to "True" if you're using the Hauppauge HD PVR.
 	restart_source = False
 	
 	# Source pipeline for the Teradek VidiU streaming video-capture device.
-	#source_pipeline = rtmpsrc location=rtmp://localhost/live/stream-name\ live=1 ! decodebin
+	#source_pipeline = rtmpsrc location=rtmp://localhost/live/stream-name\ live=1
 	
 	sink_pipeline = ximagesink sync=false
 	#control = lirc::remote_name

stbt-tv
@@ -63,4 +63,12 @@ source_pipeline=${*:-$("$(dirname "$0")/stbt-config" global.source_pipeline)}
         esac)}
 
 set -x
-eval gst-launch-1.0 ${v:-} $source_pipeline ! videoconvert ! $sink sync=false
+eval gst-launch-1.0 ${v:-} \
+    $source_pipeline \
+    ! queue name=_stbt_user_data_queue max-size-buffers=0  \
+        max-size-bytes=0 max-size-time=1000000000 \
+    ! decodebin \
+    ! videoconvert \
+    ! video/x-raw,format=BGR \
+    ! queue name=_stbt_raw_frames_queue max-size-buffers=2 leaky=upstream \
+    ! $sink sync=false

tests/run-performance-test.sh
@@ -25,6 +25,6 @@ trap "rm -f script.$$" EXIT
 
 ../stbt-run \
     --source-pipeline \
-      "filesrc location=video.mpeg ! tsdemux ! video/x-h264 ! decodebin" \
+      "filesrc location=video.mpeg ! tsdemux ! h264parse" \
     --control none \
     script.$$

tests/stbt.conf
@@ -1,5 +1,5 @@
 [global]
-source_pipeline=videotestsrc is-live=true ! video/x-raw,width=320,height=240
+source_pipeline=videotestsrc is-live=true ! video/x-raw,format=BGR,width=320,height=240
 sink_pipeline = fakesink sync=false
 control=test
 restart_source = False

tests/test-stbt-py.sh
@@ -153,7 +153,7 @@ test_that_is_screen_black_is_true_for_black_pattern() {
 	assert stbt.is_screen_black(stbt.get_frame())
 	EOF
     stbt run -v \
-        --source-pipeline 'videotestsrc pattern=black' \
+        --source-pipeline 'videotestsrc pattern=black is-live=true ! video/x-raw,format=BGR' \
         test.py
 }
 
@@ -163,7 +163,7 @@ test_that_is_screen_black_is_false_for_smpte_pattern() {
 	assert not stbt.is_screen_black(stbt.get_frame())
 	EOF
     stbt run -v \
-        --source-pipeline 'videotestsrc pattern=smpte is-live=true' \
+        --source-pipeline 'videotestsrc pattern=smpte is-live=true ! video/x-raw,format=BGR' \
         test.py
 }
 
@@ -176,7 +176,7 @@ test_that_is_screen_black_is_true_for_smpte_pattern_when_masked() {
 	)
 	EOF
     stbt run -v \
-        --source-pipeline 'videotestsrc pattern=smpte is-live=true' \
+        --source-pipeline 'videotestsrc pattern=smpte is-live=true ! video/x-raw,format=BGR' \
         test.py
 }
 
@@ -257,7 +257,7 @@ test_save_video() {
 	EOF
     set_config run.save_video "" &&
     timeout 10 stbt run -v --control none \
-        --source-pipeline 'filesrc location=video.webm ! decodebin' \
+        --source-pipeline 'filesrc location=video.webm' \
         test.py
 }
 

commit 74ca0973e1e70c9c40f12f186701bde01162d8a7

Bug fix: Ensure that frames are still dropped even after a pipeline restart
If stbt's image analysis is slower than real time frames can get queued
up in the pipeline meaning that the frames we are operating on may be out
of date.  To minimise this latency we drop frames from a queue in the
GStreamer pipeline.

This commit fixes a bug introduced in b8d39d4 where this leaky behaviour
was not being applied after the pipeline was restarted.

stbt.py
@@ -1187,14 +1187,6 @@ class Display(object):
         debug("source pipeline: %s" % self.source_pipeline_description)
         debug("sink pipeline: %s" % sink_pipeline_description)
 
-        if (self.source_pipeline.set_state(Gst.State.PAUSED)
-                == Gst.StateChangeReturn.NO_PREROLL):
-            ddebug('This is a live source, dropping frames if we get behind')
-            self.source_pipeline.get_by_name('_stbt_raw_frames_queue') \
-                .set_property('leaky', 'downstream')
-            self.source_pipeline.get_by_name('appsink') \
-                .set_property('sync', False)
-
         self.source_pipeline.set_state(Gst.State.PLAYING)
         self.sink_pipeline.set_state(Gst.State.PLAYING)
 
@@ -1222,6 +1214,14 @@ class Display(object):
             self.start_timestamp = None
             source_queue.connect("underrun", self.on_underrun)
             source_queue.connect("running", self.on_running)
+
+        if (source_pipeline.set_state(Gst.State.PAUSED)
+                == Gst.StateChangeReturn.NO_PREROLL):
+            ddebug('This is a live source, dropping frames if we get behind')
+            source_pipeline.get_by_name('_stbt_raw_frames_queue') \
+                .set_property('leaky', 'downstream')
+            source_pipeline.get_by_name('appsink') \
+                .set_property('sync', False)
 
         self.source_pipeline = source_pipeline
 