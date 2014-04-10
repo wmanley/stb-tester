2b0b9413ecc23f1cdab3a8dd4f28e21deeb30fec
William Manley <will@stb-tester.com>
2014-04-10 11:16:16 +0100

    tess-train: Don't spew files all over the place

--- tests/ocr/training/tess-train.py
+++ tests/ocr/training/tess-train.py
@@ -5,8 +5,10 @@
 import argparse
 import subprocess
 import sys
+import tempfile
 from textwrap import dedent
 from tempfile import NamedTemporaryFile
+from shutil import rmtree
 import os
 from collections import namedtuple
 
@@ -63,61 +65,80 @@ def main(argv):
                           flags=flags))
         
 
-    for font in fonts:
-        # Generate box files
-        with NamedTemporaryFile() as text:
-            text.write(example_text.encode('utf-8'))
-            text.flush()
-            subprocess.check_call(['cat', text.name])
-            subprocess.check_call([
-                'text2image',
-                '--text=%s' % text.name,
-                '--outputbase=%s' % font.base,
-                '--font=%s' % font.name,
-                '--fonts_dir=%s' % os.path.dirname(font.filename)
-                ])
+    outfile = os.path.abspath("%s.traineddata" % args.lang)
 
-        subprocess.check_call([
-            'tesseract',
-            '%s.tif' % font.base,
-            font.base,
-            'box.train.stderr'])
-
-    subprocess.check_call(
-        ['unicharset_extractor'] +
-        ['%s.box' % font.base for font in fonts])
-
-    subprocess.check_call([
-        'set_unicharset_properties',
-        '-U', 'unicharset', '-O', 'unicharset', '--script_dir=%s' % langdata])
-
-    with open('font_properties', 'w') as f:
+    tmpdir = tempfile.mkdtemp()
+    try:
+        os.chdir(tmpdir)
         for font in fonts:
-            f.write("%s %i %i %i %i %i\n" % (
-                font.id,
-                font.flags & FontFlags.ITALIC and 1,
-                font.flags & FontFlags.BOLD and 1,
-                font.flags & FontFlags.FIXED and 1,
-                font.flags & FontFlags.SERIF and 1,
-                font.flags & FontFlags.FRAKTUR and 1))
+            # Generate box files
+            with NamedTemporaryFile() as text:
+                text.write(example_text.encode('utf-8'))
+                text.flush()
+                subprocess.check_call([
+                    'text2image',
+                    '--text=%s' % text.name,
+                    '--outputbase=%s' % font.base,
+                    '--font=%s' % font.name,
+                    '--fonts_dir=%s' % os.path.dirname(font.filename),
+                    '--degrade_image=false',
+                    '--xsize=1280',
+                    '--ysize=720',
+                    '--ptsize=6'
+                    ])
 
-    if args.shape_clustering:
+            subprocess.check_call([
+                'tesseract',
+                '%s.tif' % font.base,
+                font.base,
+                'box.train.stderr'])
+
+        subprocess.check_call(
+            ['unicharset_extractor'] +
+            ['%s.box' % font.base for font in fonts])
+
         subprocess.check_call([
-            'shapeclustering',
+            'set_unicharset_properties',
+            '-U', 'unicharset', '-O', 'unicharset', '--script_dir=%s' % langdata])
+
+        with open('font_properties', 'w') as f:
+            for font in fonts:
+                f.write("%s %i %i %i %i %i\n" % (
+                    font.id,
+                    font.flags & FontFlags.ITALIC and 1,
+                    font.flags & FontFlags.BOLD and 1,
+                    font.flags & FontFlags.FIXED and 1,
+                    font.flags & FontFlags.SERIF and 1,
+                    font.flags & FontFlags.FRAKTUR and 1))
+
+        if args.shape_clustering:
+            subprocess.check_call([
+                'shapeclustering',
+                '-F', 'font_properties',
+                '-U', 'unicharset'] +
+                ['%s.tr' % f.base for f in fonts])
+
+        subprocess.check_call([
+            'mftraining',
             '-F', 'font_properties',
-            '-U', 'unicharset'] +
+            '-U', 'unicharset',
+            '-O', '%s.unicharset' % args.lang] +
             ['%s.tr' % f.base for f in fonts])
+        assert os.path.exists('%s.unicharset' % args.lang)
 
-    subprocess.check_call([
-        'mftraining',
-        '-F', 'font_properties',
-        '-U', 'unicharset',
-        '-O', '%s.unicharset' % args.lang] +
-        ['%s.tr' % f.base for f in fonts])
-    assert os.path.exists('%s.unicharset' % args.lang)
+        subprocess.check_call(['cntraining'] + ['%s.tr' % f.base for f in fonts])
 
-    subprocess.check_call(['cntraining'] + ['%s.tr' % f.base for f in fonts])
-    subprocess.check_call(['combine_tessdata', '%s.' % args.lang])
+        os.rename('inttemp', '%s.inttemp' % args.lang)
+        os.rename('pffmtable', '%s.pffmtable' % args.lang)
+        os.rename('shapetable', '%s.shapetable' % args.lang)
+        os.rename('font_properties', '%s.font_properties' % args.lang)
+        os.rename('normproto', '%s.normproto' % args.lang)
+
+        subprocess.check_call(['combine_tessdata', '%s.' % args.lang])
+        os.rename('%s.traineddata' % args.lang, outfile)
+    finally:
+        print tmpdir
+#        rmtree(tmpdir)
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
